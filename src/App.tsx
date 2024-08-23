6baa20d8003dbaea5894e7e92e03395e91e75442
Sofia Bargues <barguessofia@gmail.com>
2024-08-23 05:11:28 +0100

    redo and undo works

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,59 @@
 import { useState } from "react";
 
 function App() {
-  const [clickX, setClickX] = useState(0);
-  const [clickY, setClickY] = useState(0);
   const [listOfDots, setListOfDots] = useState<{ x: number; y: number }[]>([]);
-
+  const [deletedDots, setDeletedDots] = useState<{ x: number; y: number }[]>(
+    []
+  );
   function handleReset() {
     setListOfDots([]);
+    setDeletedDots([]);
   }
+
+  function handleUndo() {
+    //borrar de listOfDots el ultimo elemento
+    let list = [...listOfDots];
+    let deleted = list.pop();
+    setListOfDots(list);
+
+    //agregar el elemento eliminado a deletedDots
+    let listDeleted = [...deletedDots];
+    listDeleted.push(deleted);
+    setDeletedDots(listDeleted);
+  }
+
+  function handleRedo() {
+    let listDeleted = [...deletedDots];
+    //saco el ultimo elemento de deletedDots
+    let deleted = listDeleted.pop();
+    setDeletedDots(listDeleted);
+    //sumo el ultimo elemento a listOfDots
+    let list = [...listOfDots];
+    list.push(deleted);
+    setListOfDots(list);
+  }
+
   function handleClick(event) {
     const x = event.clientX - 20;
     const y = event.clientY - 80;
 
-    setClickX(x);
-    setClickY(y);
     setListOfDots((prevDots) => [...prevDots, { x, y }]);
   }
-  console.log(listOfDots);
   return (
     <div className="bg-red-500 flex justify-center h-screen relative">
       <div className="bg-white w-[950px] h-[900px] m-auto ">
         {" "}
         <div>
-          <button className="bg-green-500 m-2 p-2 rounded-md text-white">
+          <button
+            onClick={handleUndo}
+            className="bg-green-500 m-2 p-2 rounded-md text-white"
+          >
             Undo
           </button>
-          <button className="bg-green-500 m-2 p-2 rounded-md text-white">
+          <button
+            onClick={handleRedo}
+            className="bg-green-500 m-2 p-2 rounded-md text-white"
+          >
             Redo
           </button>
           <button
